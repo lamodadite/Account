4e3b64cf5b96c47eb1979365469cac75f0513312
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-08-22 22:27:08 +0900

    mockito

---
+++ src/test/java/com/example/account/service/AccountServiceTest.java
@@ -0,0 +1,52 @@
+package com.example.account.service;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.AccountStatus;
+import com.example.account.repository.AccountRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTest {
+    @Mock
+    private AccountRepository accountRepository;
+    @InjectMocks
+    private AccountService accountService;
+
+    @Test
+    @DisplayName("계좌 조회 성공")
+    void testXXX() {
+        //given
+        given()
+        //when
+        //then
+    }
+
+    @Test
+    @DisplayName("Test 이름 변경")
+    void testGetAccount() {
+        Account account = accountService.getAccount(2L);
+
+        assertEquals("40000", account.getAccountNumber());
+        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
+    }
+
+    @Test
+    void testGetAccount2() {
+        Account account = accountService.getAccount(2L);
+
+        assertEquals("40000", account.getAccountNumber());
+        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
+    }
+
+
+}
